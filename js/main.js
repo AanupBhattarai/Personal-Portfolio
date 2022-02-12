83577b34e8f39a97f1e1da303bfa14cac2202398
Anup Bhattarai <aanupbhattarai.np@gmail.com>
2022-02-12 14:20:13 +0545

    Added till My Services Section

---
+++ js/main.js
@@ -0,0 +1,6 @@
+const parallax = document.getElementById("parallax");
+
+window.addEventListener("scroll", function () {
+  let offset = window.pageYOffset;
+  parallax.style.backgroundPositionY = offset * 0.7 + "px";
+});
